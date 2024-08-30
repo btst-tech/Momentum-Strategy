fec7f7b36a44a966b8f0f9b6b084cf2dcc45c82c
Vinay Bajaj <96784641+Vinay-Bajaj-1@users.noreply.github.com>
2024-08-30 16:07:34 +0530

    api key for smartapi (angelone)

---
+++ config.py
@@ -0,0 +1,7 @@
+#angelone smart api
+api_key_smartapi = '6y72sfx7 '
+secret_key_smartapi = '92f0ce4e-765d-4f86-a891-3b468f166d06'
+angel_one_pin = '1100'
+qr_smartApi = 'D5MBZSQJHYFD3VYCODH3YV5CFU'
+client_id = 'V57664223'
+angel_one_username = 'V57664223'
